5495803085d703f378473dd09394707ec159faa8
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2025-10-26 22:29:49 +0530

    initial commit

---
+++ tutorial/main.py
@@ -0,0 +1,32 @@
+# import the relevant modules
+import pyautogui
+import time
+pyautogui.FAILSAFE = False
+
+# sleep
+time.sleep(3)
+# my screen resolution Size(width=1710, height=1112)
+# Mouse Functions
+#print(pyautogui.size()) # prints the resolution of the screen
+# close resolution Point(x=18, y=54)
+# print(pyautogui.position()) #position of the pointer
+
+# moving the mouse
+
+pyautogui.moveTo(0,0,3)
+
+pyautogui.moveTo(18,54,3)
+
+#clicking
+pyautogui.click() #pyautogui.click(26,65,number of clicks). "YOU CAN GIVE COORDINATES OR USE movTo to take the pointer to the point u want"
+
+# left click and right click
+# pyautogui.leftClick()
+# pyautogui.rightClick()
+# number of clicks
+# pyautogui.doubleClick() 
+# pyautogui.tripleClick()
+
+
+# scrolling
+# pyautogui.scroll(-500)
